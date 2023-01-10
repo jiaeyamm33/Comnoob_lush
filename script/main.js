982d84a7c6ee06d433ed38b8ac43d07ccd81c3fc
kbykor <quddud33@gmail.com>
2023-01-10 16:34:46 +0900

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,11 +1,13 @@
 $(function(){
   //메인 휠 이벤트
   let stop_scroll = 0;
-  window.addEventListener('wheel',function(e){
-    if($(this).scrollTop() != 0 && e.deltaY < 0){
+  $(window).on('load', function(e){ // 페이지가 로딩되면
+    if($(this).scrollTop() != 0){ // 현재 스크롤을 검증하여 
       stop_scroll = 1;
-    }    
-    if(stop_scroll == 0 && e.deltaY > 0){
+    }
+  });
+  window.addEventListener('wheel',function(e){
+    if(stop_scroll == 0 && e.deltaY > 0 && $(this).scrollTop() == 0){
       e.preventDefault();
       $('html, body').animate({scrollTop: $('#main_video_box').height()}, 'slow');
       stop_scroll = 1;

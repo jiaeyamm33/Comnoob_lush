ab7f3aa953b05be6d120b7c6c1d66eb0d4858be2
jiaeyamm33 <jiaeyamm33@gmail.com>
2023-01-11 17:47:15 +0900

    sw 수정

--- script/member.js
+++ script/member.js
@@ -8,9 +8,15 @@ logbtn.addEventListener('click', function() {
     id = document.getElementById('username').value;
     pw = document.getElementById('l_password').value;
 
-    if((id == 'test') && (pw == '1234')) {
-        
-        swal("로그인 성공!", 'success')
-        
+    if(id == 'test') {
+        if(pw == '1234') {
+            swal('로그인 성공', '로그인 되었습니다.', 'success').delay(2000).location.href= 'index.html';
+        } else {
+            swal('패스워드 오류', '비밀번호를 확인해주세요.', 'error');
+        } 
+    } else if((id == '') || (pw == '')) {
+        swal('입력 오류', '아이디나 패스워드를 입력해주세요.', 'error');
+    } else {
+        swal('아이디 오류', '아이디를 확인해주세요.', 'error');
     }
 })

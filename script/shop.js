dc86fc92a373fb82f7d00969143c3c2e12aa3cbe
aaron9909@naver.com <aaron9909@naver.com>
2023-01-12 09:35:17 +0900

    array

--- script/shop.js
+++ script/shop.js
@@ -1,20 +1,20 @@
 /*
 let array = [
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'},
-  {'name' : '인터갈릭틱', 'category' : '배스 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : '?.png'}
+  {'name' : '인터갈릭틱', 'category' : '배쓰 밤', 'price' : '17000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product01.png'},
+  {'name' : '럭스', 'category' : '배쓰 밤', 'price' : '20000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product05.png'},
+  {'name' : '슬리피', 'category' : '샤워 젤', 'price' : '19000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product09.png'},
+  {'name' : '해피 페이스', 'category' : '배쓰 밤', 'price' : '15000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product13.png'},
+  {'name' : '트와일라잇', 'category' : '슬리피 보디 스프레이', 'price' : '60000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product02.png'},
+  {'name' : '챠콜', 'category' : '페이셜 솝', 'price' : '22000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product06.png'},
+  {'name' : '더 컴포터', 'category' : '배쓰 밤', 'price' : '19000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product10.png'},
+  {'name' : '대디-오', 'category' : '샴푸', 'price' : '19000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product14.png'},
+  {'name' : '마스크 오브 매그너민티', 'category' : '파워 마스크', 'price' : '22000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product03.png'},
+  {'name' : '더티 보디 스프레이', 'category' : '보디 스프레이', 'price' : '60000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product07.png'},
+  {'name' : '더티 로즈', 'category' : '배쓰 밤', 'price' : '18000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product11.png'},
+  {'name' : '릴렉스', 'category' : '기프트', 'price' : '70000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product15.png'},
+  {'name' : '섹스 밤', 'category' : '배쓰 밤', 'price' : '14000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product04.png'},
+  {'name' : '팬시', 'category' : '향수', 'price' : '55000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product08.png'},
+  {'name' : '스노우 페어리 4 윅', 'category' : '캔들', 'price' : '100000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product12.png'},
+  {'name' : '말차 롤', 'category' : '버블 바', 'price' : '20000', 'like' : 5, 'view' : 50, 'src' : './images/shop/product16.png'}
 ];
 */
